60576350078a90f54740f904954c73c804a623ef
ckldev <ckldev@users.noreply.github.com>
2017-10-02 23:47:34 +0800

    Update MainActivity.java
    getSharedPreferences() method compatible to android 7.0/7.1/8.0

--- app/src/main/java/com/lanyus/blocksecureflag/MainActivity.java
+++ app/src/main/java/com/lanyus/blocksecureflag/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lanyus.blocksecureflag;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -30,16 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
-        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_WORLD_READABLE + MODE_WORLD_WRITEABLE);
+        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", Context.MODE_PRIVATE);
         checkedApps = JSON.parseArray(sharedPreferences.getString("checkedApps", JSON.toJSONString(checkedApps)), CheckedApp.class);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(new RecycleViewDivider(MainActivity.this, LinearLayoutManager.HORIZONTAL));
         recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, checkedApps);
         recyclerView.setAdapter(recyclerViewAdapter);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean save() {
-        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_WORLD_READABLE + MODE_WORLD_WRITEABLE);
+        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("checkedApps", JSON.toJSONString(checkedApps));
         return editor.commit();
